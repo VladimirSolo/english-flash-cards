4e8b87c7744b19c44e5be1c7cd86ec80682fd5eb
Vladimir Solo <freeman.wushu@mail.ru>
2023-01-10 23:17:08 +0600

    add seed user

---
+++ db/seeders/20230110161211-CreateUsers.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    const testUser = {
+      name: 'test',
+      login: 'Solo Solovey',
+      password: await bcrypt.hash('qwerty', 10),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    // console.log(testUser);
+    await queryInterface.bulkInsert(['Users',
+      testUser,
+    ]);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Users');
+  },
+};
