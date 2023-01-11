d010627d57ebaaea729b66fd6f714d32d611bb86
Vladimir Solo <freeman.wushu@mail.ru>
2023-01-12 00:26:33 +0600

    refactor wordshow component add btn

--- views/WordShow.jsx
+++ views/WordShow.jsx
@@ -2,14 +2,22 @@ const React = require('react');
 const Layout = require('./Layout');
 
 function WordShow({ words, user }) {
+
   return (
+
     <Layout user={user}>
 
       <div className="wrapper" key={words.id}>
         {words.map((word) => (
-          <div className="wrapper_card">
-            <p className="front">{word.title_eng}</p>
-            <p className="back">{word.title_rus}</p>
+          <div className="wrapper_card" data-id={word.id}>
+            <p className="front rotate">
+              {word.title_eng}
+              <button type="button" className="btnDelete btn btn-warning">done</button>
+            </p>
+            <p className="back rotate">
+              {word.title_rus}
+              <button type="button" className="btnDelete btn btn-warning">done</button>
+            </p>
           </div>
         ))}
       </div>
