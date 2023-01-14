22f5106bc2e6bbe73a0e53e4e62fcaa06e0f95da
Vladimir Solo <freeman.wushu@mail.ru>
2023-01-14 18:39:20 +0600

    refactore funtional of progressbar

--- public/js/progressbar.js
+++ public/js/progressbar.js
@@ -1,18 +1,17 @@
-const circle = document.querySelector('.progress_circle');
-const radius = circle.r.baseVal.value;
-const lengthCircle = 2 * Math.PI * radius;
-// numbers
-const trueStatus = document.querySelector('.trueStatus');
-const falseStatus = document.querySelector('.falseStatus');
-const sum = Number(trueStatus.textContent) + Number(falseStatus.textContent);
-const perc = Math.floor((trueStatus.textContent * 100) / sum);
-// add numbers to style
-circle.style.strokeDasharray = `${lengthCircle} ${lengthCircle}`;
-circle.style.strokeDashoffset = lengthCircle;
-// function find percent
-function setProgress(percent) {
-  const offset = lengthCircle - (percent / 100) * lengthCircle;
-  circle.style.strokeDashoffset = offset;
-}
+const circle = document.querySelectorAll('.progress_circle');
+const text = document.querySelectorAll('text');
 
-setProgress(perc);
+circle.forEach((el, i) => {
+  // take radius
+  const radius = el.r.baseVal.value;
+  // find circumference
+  const lengthCircle = 2 * Math.PI * radius;
+  // add new value in style
+  el.style.strokeDasharray = `${lengthCircle} ${lengthCircle}`;
+  // console.log( el.style.strokeDasharray)
+  el.style.strokeDashoffset = lengthCircle;
+  // find part of circle
+  const offset = lengthCircle - (text[i].textContent.slice(0, -1) / 100) * lengthCircle;
+  // add new value in style
+  el.style.strokeDashoffset = offset;
+});
