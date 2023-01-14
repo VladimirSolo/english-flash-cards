31bd8993bec51d9e118603feee7a74855495f0aa
Irina <irina.danilenko@gmail.com>
2023-01-14 16:12:27 +0300

    header

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -1,4 +1,4 @@
-const React = require('react');
+const React = require("react");
 
 function Layout({ children, user }) {
   return (
@@ -9,38 +9,86 @@ function Layout({ children, user }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>LearnYourEnglish</title>
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossOrigin="anonymous" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+          crossOrigin="anonymous"
+        />
+
         <link rel="stylesheet" href="/css/style.css" />
         {/* <script defer src="/js/application.js" /> */}
         {/* <script defer src="/js/addList.js" /> */}
+        <script defer
+          src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+          integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+          crossOrigin="anonymous"
+        />
+        <script defer
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+          integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
+          crossOrigin="anonymous"
+        />
         <script defer src="/js/script.js" />
         <script defer src="/js/progressbar.js" />
       </head>
       <body>
-        {
-          user
-            ? (
-              <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                  <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                      <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/list"> Home </a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/user"> {user.name} </a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/logout">LogOut</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </nav>
-            ) : (
-              <div>Hello, World!</div>
-            )
-        }
+        {user ? (
+          <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="#">
+                LearnYourEnglish
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon" />
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a
+                      className="nav-link active"
+                      aria-current="page"
+                      href="/list"
+                    >
+                      {" "}
+                      Home{" "}
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link active"
+                      aria-current="page"
+                      href="/user"
+                    >
+                      {" "}
+                      {user.name}{" "}
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link active"
+                      aria-current="page"
+                      href="/logout"
+                    >
+                      LogOut
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        ) : (
+          <div>Hello, World!</div>
+        )}
         <div className="container">{children}</div>
       </body>
     </html>
